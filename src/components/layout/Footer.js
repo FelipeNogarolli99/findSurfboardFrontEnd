9500aca3d02091fe4a7e98f4d57ca29773901481
Felipe Nogarolli Macedo <felipenogarolli@hotmail.com>
2025-01-29 15:48:23 -0300

    Resolvendo conflitos e sincronizando com o remoto

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+function Footer(){
+
+    return(
+     <footer>
+        <p>
+            <span className="bold"> Find a Board</span> &copy; 2025 
+        </p>
+     </footer>
+    )
+}
+
+export default Footer
